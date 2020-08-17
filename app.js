06af295c7093b07dd0d2a326c6e03218fe08f84f
Michael Soohoo <Soohoo@MacBook-Pro.fios-router.home>
2020-08-17 16:31:55 -0400

    basic structure restaurants almost done

--- app.js
+++ app.js
@@ -1,40 +1,54 @@
 const randomTenNames = []
 
 const restaurantURL = 'https://data.cityofnewyork.us/resource/pitm-atqc.json?borough='
-const getRestaurantNames = async (borough) => {
+const getRestaurantData = async (borough) => {
   try {
     // Making call to the NYC Open Data - Open Streets Program
     const response = await axios.get(restaurantURL + borough)
 
-    const mainRestaurantDiv = document.querySelector('.restaurantNames')
-
+    const mainRestaurantDiv = document.querySelector('.restaurantsInBorough')
     for (let i = 0; i <= 10; i++) {
-      console.log(response.data[i])
+      const divSection = document.createElement('div')
+      mainRestaurantDiv.append(divSection)
+    }
 
-      const name = document.createElement('div')
-      name.textContent = response.data[i].restaurant_name
-      mainRestaurantDiv.append(name)
-
+    const mainRestaurantNameDiv = document.querySelectorAll('.restaurantsInBorough > div')
+    for (let i = 0; i <= 10; i++) {
+      const nameOfRestaurant = document.createElement('p')
       const businessAddress = document.createElement('p')
-      businessAddress.textContent = response.data[i].business_address
-      mainRestaurantDiv.append(businessAddress)
-
       const servesAlcohol = document.createElement('p')
-      servesAlcohol.textContent = response.data[i].qualify_alcohol
-      mainRestaurantDiv.append(servesAlcohol)
+      const typeOfSeating = document.createElement('p')
+
+      nameOfRestaurant.textContent = response.data[i].restaurant_name
+      mainRestaurantNameDiv[i].append(nameOfRestaurant)
+      nameOfRestaurant.classList.add("restaurant-name");
+
+      businessAddress.textContent = response.data[i].business_address
+      mainRestaurantNameDiv[i].append(businessAddress)
+      businessAddress.classList.add("business-address");
+
+      if (response.data[i].qualify_alcohol === 'yes') {
+        servesAlcohol.textContent = "Serves alcohol: Yes"
+        mainRestaurantNameDiv[i].append(servesAlcohol)
+        servesAlcohol.classList.add("serves-alcohol");
+      } else {
+        servesAlcohol.textContent = "Serves alcohol: No"
+        mainRestaurantNameDiv[i].append(servesAlcohol)
+        servesAlcohol.classList.add("serves-alcohol");
+      }
 
       if (response.data[i].approved_for_roadway_seating === "yes" && response.data[i].approved_for_sidewalk_seating === "yes") {
-        const typeOfSeating = document.createElement('p')
-        typeOfSeating.textContent = "Both"
-        mainRestaurantDiv.append(`Sidewalk or Roadway Seating: ${typeOfSeating.textContent}`)
+        typeOfSeating.textContent = "Sidewalk or Roadway Seating: Both"
+        mainRestaurantNameDiv[i].append(typeOfSeating)
+        typeOfSeating.classList.add("seating-type");
       } else if (response.data[i].approved_for_roadway_seating === "yes" && response.data[i].approved_for_sidewalk_seating === "no") {
-        const typeOfSeating = document.createElement('p')
-        typeOfSeating.textContent = "Roadway Seating Only"
-        mainRestaurantDiv.append(`Sidewalk or Roadway Seating: ${typeOfSeating.textContent}`)
+        typeOfSeating.textContent = "Sidewalk or Roadway Seating: Roadway Seating Only"
+        mainRestaurantNameDiv[i].append(typeOfSeating)
+        typeOfSeating.classList.add("seating-type");
       } else {
-        const typeOfSeating = document.createElement('p')
-        typeOfSeating.textContent = "Sidewalk Seating Only"
-        mainRestaurantDiv.append(`Sidewalk or Roadway Seating: ${typeOfSeating.textContent}`)
+        typeOfSeating.textContent = "Sidewalk or Roadway Seating:Sidewalk Seating Only"
+        mainRestaurantNameDiv[i].append(typeOfSeating)
+        typeOfSeating.classList.add("seating-type");
       }
     }
 
@@ -43,4 +57,4 @@ const getRestaurantNames = async (borough) => {
   }
 }
 
-getRestaurantNames('Manhattan')
+getRestaurantData('Queens')
