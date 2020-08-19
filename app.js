b92cddbca6a9640d26554c97f7e2a76b75ba3837
Michael Soohoo <Soohoo@MacBook-Pro.fios-router.home>
2020-08-18 21:13:38 -0400

    cors solved

--- app.js
+++ app.js
@@ -3,6 +3,22 @@ const newsURL = 'https://newsapi.org/v2/everything?apiKey=45bbd722e9ed4d88887103
 
 const getRestaurantData = async (borough) => {
   try {
+
+    // This is taken from https://github.com/Rob--W/cors-anywhere/#documentation and help from Raul
+    const newsUrl = 'http://cors-anywhere.herokuapp.com/https://newsapi.org/v2/everything?apiKey=45bbd722e9ed4d88887103aa39f34a4f&q=covid-19'
+
+    const responseNewsUrl = await axios.get(newsUrl)
+    console.log(responseNewsUrl.data)
+
+    // Creating div that will hold news articles
+    const covidNews = document.querySelector('.covid-news')
+    for (let i = 0; i < 4; i++) {
+      const covidSection = document.createElement('div')
+      covidNews.append(covidSection)
+    }
+
+
+
     // Making call to the NYC Open Data - Open Streets Program
     const response = await axios.get(restaurantURL + borough)
 
@@ -81,37 +97,3 @@ const removeLastBoroughList = () => {
     removeRestaurants.removeChild(removeRestaurants.lastChild)
   }
 }
-
-
-// Getting the latest COVID - 19 news
-const getCovidNews = async () => {
-  try {
-
-    // This is taken from https://github.com/Rob--W/cors-anywhere/#documentation
-    const covidNewsUrl = (function () {
-      var cors_api_host = 'cors-anywhere.herokuapp.com';
-      var cors_api_url = 'https://' + cors_api_host + '/' + 'https://newsapi.org/v2/everything?apiKey=45bbd722e9ed4d88887103aa39f34a4f&q=covid-19';
-      var slice = [].slice;
-      var origin = window.location.protocol + '//' + window.location.host;
-      var open = XMLHttpRequest.prototype.open;
-      XMLHttpRequest.prototype.open = function () {
-        var args = slice.call(arguments);
-        var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
-        if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
-          targetOrigin[1] !== cors_api_host) {
-          args[1] = cors_api_url + args[1];
-        }
-        return open.apply(this, args);
-      };
-    })();
-
-    // Making call to the News API for articles
-    // const response = await axios.get(covidNewsUrl)
-    // console.log(response)
-
-  } catch (error) {
-    console.log(`Error; ${error}`)
-  }
-}
-
-getCovidNews()
